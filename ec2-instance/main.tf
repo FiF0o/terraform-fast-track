658e464d80d972ee0cc7931b081079b6a979d2d0
FiF0o <jonathan.lazarini@wanadoo.fr>
2022-10-04 14:13:41 +0100

    define eip and sg

--- ec2-instance/main.tf
+++ ec2-instance/main.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "ec2" {
+resource "aws_instance" "db" {
   ami = "ami-00785f4835c6acf64"
   instance_type = "t2.micro"
   tags = {
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2" {
   }
 }
 
-resource "aws_instance" "ec2" {
+resource "aws_instance" "web" {
   ami = "ami-00785f4835c6acf64"
   instance_type = "t2.micro"
   tags = {
@@ -14,3 +14,46 @@ resource "aws_instance" "ec2" {
   }
 }
 
+resource "aws_eip" "web_ip" {
+  instance = aws_instance.web.id
+}
+
+variable "ingress" {
+ type = list(number)
+ default = [ 80, 443 ]
+}
+
+variable "egress" {
+ type = list(number)
+ default = [ 80, 443 ]
+}
+
+
+resource "aws_security_group" "web_traffic" {
+  name = "Allow Web traffic"
+
+  dynamic "ingress" {
+    iterator = port
+    for_each = var.ingress
+    
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    iterator = port
+    for_each = var.egress
+    
+    content {
+      from_port = port.value
+      to_port = port.value
+      protocol = "TCP"
+      cidr_blocks = ["0.0.0.0/0"]
+    }
+  }
+
+}
