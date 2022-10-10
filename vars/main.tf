0e1c69f97c00074c5a6187f773231b598a7f104a
FiF0o <jonathan.lazarini@wanadoo.fr>
2022-10-10 18:00:04 +0100

    add multiple vars

--- vars/main.tf
+++ vars/main.tf
@@ -2,8 +2,13 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+variable "number_of_servers" {
+  type = number
+}
+
+# tf plan -var-file=test.tfvars
 resource "aws_instance" "ec2" {
   ami = "ami-00785f4835c6acf64"
   instance_type = "t2.micro"
+  count = var.number_of_servers
 }
-
