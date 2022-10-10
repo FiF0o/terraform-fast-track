affec99b950c5181c29f5d7bfa9d9720126f811c
FiF0o <jonathan.lazarini@wanadoo.fr>
2022-10-10 17:49:17 +0100

    add count ec2 db & web server

--- count/main.tf
+++ count/main.tf
@@ -2,9 +2,13 @@ provider "aws" {
   region = "eu-west-2"
 }
 
+module "db" {
+  source = "./db"
+  server_names = ["mariadb", "mysql"]
+}
+
 resource "aws_instance" "ec2" {
   ami = "ami-00785f4835c6acf64"
   instance_type = "t2.micro"
-  count = 3
+  count = 2
 }
-
